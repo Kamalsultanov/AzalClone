59abf3094320db42cd64fab6c8430dfbff08926e
Expep <89018966+Expep@users.noreply.github.com>
2025-01-19 16:35:33 +0400

    Fixes

--- src/components/header/FunctionalHeader.jsx
+++ src/components/header/FunctionalHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
-import Logo from "../../assets/icons/logo.svg";
-import DarkLogo from "../../assets/icons/darklogo.svg";
+import Logo from "/Images/logo.svg";
+import DarkLogo from "/Images/darklogo.svg";
 import { BsCurrencyExchange } from "react-icons/bs";
 import { CiGlobe } from "react-icons/ci";
 import { IoPersonOutline } from "react-icons/io5";
